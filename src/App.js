ad418d4cbbaa81a9b9f05d06d5751f1aa2ac4028
Selma Om <selma.om1@gmail.com>
2023-01-09 19:57:10 +0100

    added Navigation bar, components Home and Starred

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './components/pages/Home';
+import Starred from './components/pages/Starred';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Navs/>
+    <Switch>
+      <Route exact path="/" element={<Home/>} >
+<Home/>
+      </Route >
+      <Route exact path="/starred">
+<Starred/>
+      </Route>
+    </Switch>
+    
+   </div>
   );
 }
 
