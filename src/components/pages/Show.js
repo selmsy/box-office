fa01886b547311e666ec1c85b2d95934c8b41af2
Selma Om <selma.om1@gmail.com>
2023-01-11 22:33:35 +0100

    added Cast, Seasons, fixed bugs

--- src/components/pages/Show.js
+++ src/components/pages/Show.js
@@ -1,23 +1,55 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react';
 import {useParams} from 'react-router-dom';
-import { apiGet } from '../../misc/config';
+//import {apiGet} from '../../misc/config';
+import Cast from '../show/Cast';
+import Details from '../show/Details';
+import Seasons from '../show/Seasons';
+import ShowMainData from '../show/ShowMainData';
+import { useShow } from '../../misc/custom-hooks';
+
+
+
+
+
 const Show = () => {
     const {id} = useParams();
-const [show, setShow] = useState(null);
-    useEffect(() =>{
+    const {show, isLoading, error} = useShow(id);
 
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results =>{
-setShow(results)
-        })
-    }, [id])
-
-    console.log('show', show);
-
-  return (
-    <div>
-      Show page.
-    </div>
-  )
-}
-
-export default Show
+    if (isLoading) {
+        return <div>Data is being loaded</div>;
+      }
+    
+      if (error) {
+        return <div>Error occured: {error}</div>;
+      }
+    
+      return (
+        <div>
+          <ShowMainData
+            image={show.image}
+            name={show.name}
+            rating={show.rating}
+            summary={show.summary}
+            tags={show.genres}
+          />
+         
+    
+          <div>
+            <h2>Details</h2>
+            <Details/>
+          </div>
+    
+          <div>
+            <h2>Seasons</h2>
+            <Seasons/>
+          </div>
+    
+          <div>
+            <h2>Cast</h2>
+            <Cast/>
+          </div>
+        </div>
+      );
+    };
+    
+    export default Show;
